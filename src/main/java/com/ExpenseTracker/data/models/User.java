3e17cd2a9567738a5f10d529951d4cbf9c796276
GiftedGiftB <asekhamegift@gmail.com>
2025-10-02 03:32:52 +0100

    made some changes

--- src/main/java/com/ExpenseTracker/data/models/User.java
+++ src/main/java/com/ExpenseTracker/data/models/User.java
@@ -1,15 +1,19 @@
-package com.Tracker.data.models;
+package com.ExpenseTracker.data.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String email;
 }
