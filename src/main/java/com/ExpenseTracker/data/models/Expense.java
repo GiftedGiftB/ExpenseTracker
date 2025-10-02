388f2906979559ea25b15042441a02382499850c
GiftedGiftB <asekhamegift@gmail.com>
2025-10-02 03:39:18 +0100

    changes was made in the Expense class

--- src/main/java/com/ExpenseTracker/data/models/Expense.java
+++ src/main/java/com/ExpenseTracker/data/models/Expense.java
@@ -1,18 +1,27 @@
-package com.Tracker.data.models;
+package com.ExpenseTracker.data.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "expenses")
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    private String id;
+    private Long id;
     private String title;
     private Double amount;
     private String category;
     private LocalDate date;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 }
