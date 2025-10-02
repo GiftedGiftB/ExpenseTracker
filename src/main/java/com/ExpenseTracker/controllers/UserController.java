022693a17031cf05b189da20609052fd99529eb0
GiftedGiftB <asekhamegift@gmail.com>
2025-10-02 03:34:11 +0100

    Still testing APIs with Postman

---
+++ src/main/java/com/ExpenseTracker/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.ExpenseTracker.controllers;
+
+import com.ExpenseTracker.data.models.User;
+import com.ExpenseTracker.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(com.ExpenseTracker.service.UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+}
