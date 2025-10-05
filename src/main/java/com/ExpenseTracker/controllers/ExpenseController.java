1665967d64df6349eeb45ff3fdae08afcf8dad1a
GiftedGiftB <asekhamegift@gmail.com>
2025-10-05 01:05:10 +0100

    currently battling with this this error "java: java.lang.ExceptionInInitializerError                                          com.sun.tools.javac.code.TypeTag :: UNKNOWN"

--- src/main/java/com/ExpenseTracker/controllers/ExpenseController.java
+++ src/main/java/com/ExpenseTracker/controllers/ExpenseController.java
@@ -2,6 +2,7 @@ package com.ExpenseTracker.controllers;
 
 import com.ExpenseTracker.data.models.Expense;
 import com.ExpenseTracker.service.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -9,8 +10,10 @@ import java.util.List;
 @RequestMapping("/api/users/{userId}/expenses")
 public class ExpenseController {
 
+
     private final ExpenseService expenseService;
 
+    @Autowired
     public ExpenseController(ExpenseService expenseService) {
         this.expenseService = expenseService;
     }
