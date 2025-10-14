fd663f1d3cf3ab4119fcd2cfec07f5d4e2dad4dc
GiftedGiftB <asekhamegift@gmail.com>
2025-10-14 08:40:18 +0100

    User service method

--- src/main/java/com/ExpenseTracker/service/UserService.java
+++ src/main/java/com/ExpenseTracker/service/UserService.java
@@ -15,12 +15,10 @@ public class UserService {
 
     public User createUser(User user) {
         return userRepository.save(user);
-
     }
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
-
 
 }
