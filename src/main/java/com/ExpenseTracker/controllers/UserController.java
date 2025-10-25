7cef953d675e479fd8eb8ac77f9a09bb051e9bb5
GiftedGiftB <asekhamegift@gmail.com>
2025-10-25 22:30:40 +0100

    User class

--- src/main/java/com/ExpenseTracker/controllers/UserController.java
+++ src/main/java/com/ExpenseTracker/controllers/UserController.java
@@ -4,13 +4,16 @@ import com.ExpenseTracker.data.models.User;
 import com.ExpenseTracker.service.UserService;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
+//@CrossOrigin(origins = "*")
+
 public class UserController {
     private final UserService userService;
 
-    public UserController(com.ExpenseTracker.service.UserService userService) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
@@ -19,8 +22,16 @@ public class UserController {
         return userService.createUser(user);
     }
 
+    @PostMapping("/login")
+    public User loginUser(@RequestBody Map<String, String> loginData) {
+        String email = loginData.get("email");
+        String password = loginData.get("password");
+        return userService.loginUser(email, password);
+    }
+
     @GetMapping
     public List<User> getAllUsers(){
         return userService.getAllUsers();
     }
+
 }
