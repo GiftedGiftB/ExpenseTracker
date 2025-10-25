8d506a0aa5f93320ea375ca124aa4a80b85c1e00
GiftedGiftB <asekhamegift@gmail.com>
2025-10-25 23:20:22 +0100

    User Repository class. this class connects with the data base

--- src/main/java/com/ExpenseTracker/data/repositories/UserRepository.java
+++ src/main/java/com/ExpenseTracker/data/repositories/UserRepository.java
@@ -3,5 +3,9 @@ package com.ExpenseTracker.data.repositories;
 import com.ExpenseTracker.data.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndPassword(String email, String password);
 }
