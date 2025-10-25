68c2d88ace46097451e848116106b02cf1239285
GiftedGiftB <asekhamegift@gmail.com>
2025-10-25 22:47:55 +0100

    User Service class

--- src/main/java/com/ExpenseTracker/service/UserService.java
+++ src/main/java/com/ExpenseTracker/service/UserService.java
@@ -4,6 +4,7 @@ import com.ExpenseTracker.data.models.User;
 import com.ExpenseTracker.data.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,11 +15,19 @@ public class UserService {
     }
 
     public User createUser(User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already registered");
+        }
         return userRepository.save(user);
     }
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    public User loginUser(String email, String password) {
+        Optional<User> foundUser = userRepository.findByEmailAndPassword(email, password);
+        return foundUser.orElseThrow(() -> new RuntimeException("Invalid email or password"));
     }
 
 }
